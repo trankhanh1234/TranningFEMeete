0510b0f6204d7d664648c3b34ce9bd006e12c263
khanhtrandth@gmail.com <khanhtrandth@gmail.com>
2021-05-28 19:27:21 +0700

    update card

--- js/main.js
+++ js/main.js
@@ -1,57 +1,39 @@
-const dataVocher = [
+const listVoucher = [
   {
-    id: 1,
-    img: "",
-    name: "",
-    content: "",
-    address: "",
-    voucher: "",
-    status: "Chưa dùng",
+    id: "1",
+    src: "https://media.meete.co/cache/0x0/2021/05/18/6a1ce3df-83dd-4ff7-9a4a-80021ab8d104.jpeg",
+    store: "Chicken Plus",
+    voucher: "89k Combo Cánh Gà Cho 2 - 3 Người",
+    tag: ["Ẩm thực", "Ăn"],
+    address: "412 Nguyễn Thị Thập - Quận 7",
+    status: false,
   },
   {
-    id: 2,
-    img: "",
-    name: "",
-    content: "",
-    address: "",
-    voucher: "",
-    status: "Đã dùng",
+    id: "2",
+    src: "https://media.meete.co/cache/0x0/2019/10/10/96cc4866-36f8-45b5-a70d-0781911a3ea8.png",
+    store: "ToCoToCo",
+    voucher: "Đồng Giá 32K Toàn Menu Size L",
+    tag: ["Uống", "Trà sữa", "Ẩm thực"],
+    address: "58 Cao Thắng - Quận 3",
+    status: true,
   },
   {
-    id: 3,
-    img: "",
-    name: "",
-    content: "",
-    address: "",
-    voucher: "",
-    status: "Đã dùng",
+    id: "3",
+    src: "https://media.meete.co/cache/0x0/2021/04/06/52a2d903-4575-4d84-9c27-b7b960673bf3.jpeg",
+    store: "Vua Đậu - Bún Đậu Nhà Vua",
+    voucher: "Giảm 20% Toàn Menu",
+    tag: ["Ăn", "Ẩm thực"],
+    address: "231 Nguyễn Thị Nhỏ - Tân Bình",
+    status: false,
   },
   {
-    id: 4,
-    img: "",
-    name: "",
-    content: "",
-    address: "",
-    voucher: "",
-    status: "Chưa dùng",
-  },
-  {
-    id: 5,
-    img: "",
-    name: "",
-    content: "",
-    address: "",
-    voucher: "",
-    status: "Đã dùng",
-  },
-  {
-    id: 6,
-    img: "",
-    name: "",
-    content: "",
-    address: "",
-    voucher: "",
-    status: "Đã dùng",
+    id: "4",
+    src: "https://media.meete.co/cache/0x0/2020/12/19/ac9e6fa0-eaab-4811-9187-bcb1ffdcdf8a.jpeg",
+    store: "Yangs Cupbop",
+    voucher: "Giảm 50% Toàn Menu Món Hàn Quốc Chính Hiệu",
+    tag: ["Ăn", "Ẩm thực"],
+    address: "51 Song Hành - Quận 2",
+    status: true,
   },
 ];
 
@@ -60,12 +42,9 @@ const statusVocher = ["Đã dùng", "chưa dùng"];
 var voucher = document.getElementById("listProduct");
 
 function getListVocher() {
-  dataVocher.forEach((item) => {
+  listVoucher.forEach((item) => {
     console.log(item.id);
-    voucher.innerHTML +=
-      `<a id="` +
-      item.id +
-      `" class="w-100">
+    voucher.innerHTML += `<a id="${item.id}" class="w-100">
     <div class="mt-container-item-deal bg-white my-2 rounder">
       <div class="mt-top-info">
         <img
@@ -99,7 +78,7 @@ function getListVocher() {
               />
             </svg>
             <p>
-              Tặng Ngay 1 Đậu Hủ Sả Ớt Với Hóa Đơn Trên 100k
+            ${item.voucher}
             </p>
           </div>
           <div
@@ -117,7 +96,7 @@ function getListVocher() {
                 d="M15.5 8.516a7.5 7.5 0 1 1-9.462-7.24A1 1 0 0 1 7 0h2a1 1 0 0 1 .962 1.276 7.503 7.503 0 0 1 5.538 7.24zm-3.61-3.905L6.94 7.439 4.11 12.39l4.95-2.828 2.828-4.95z"
               />
             </svg>
-            <p>Tiệm Trà Thanh Xuân - Huỳnh Văn Bánh</p>
+            <p>${item.store}</p>
           </div>
         </div>
       </div>
@@ -132,9 +111,7 @@ function getListVocher() {
       >
         Ngày đặt hàng 25-05-2020
       </div>
-      <div class="mt-status-deal">` +
-      item.status +
-      `</div>
+      <div class="mt-status-deal">${item.status ? "đã dùng" : "Chưa dùng"}</div>
     </div>
   </a>`;
   });
